2970fe098bdfa1956f477db9cee64aaaa90a9288
FdelMazo <FdelMazo@fi.uba.ar>
2022-11-17 20:11:11 -0300

    Make one scenario to rule them all

--- perf/run.sh
+++ perf/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export TARGET=http://$(cat ../node/lb_dns)
-export DATADOG_API_KEY=b03559c436823f4d51d6e33d4d5d36dd
-export DEBUG=plugin:publish-metrics:datadog-statsd
-npm run artillery run $1.yaml
+export DATADOG_API_KEY=""
+export DEBUG=http,http:response,plugin:publish-metrics:datadog-statsd
+export ENDPOINT=${1:-"/"}
+npm run artillery run scenario.yaml
